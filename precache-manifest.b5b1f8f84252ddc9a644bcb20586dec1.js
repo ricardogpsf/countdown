f1acde379a537e8c85bf41057967b5a9277b5eb2
Ricardo Galeno <ricardogaleno.rg@gmail.com>
2018-11-05 14:58:58 +0000

    Updates

---
+++ precache-manifest.b5b1f8f84252ddc9a644bcb20586dec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08800195382208a303b6",
+    "url": "/countdown/static/js/runtime~main.08800195.js"
+  },
+  {
+    "revision": "90d8848f0dba81e00865",
+    "url": "/countdown/static/js/main.90d8848f.chunk.js"
+  },
+  {
+    "revision": "a93cec8a557ee382682a",
+    "url": "/countdown/static/js/1.a93cec8a.chunk.js"
+  },
+  {
+    "revision": "90d8848f0dba81e00865",
+    "url": "/countdown/static/css/main.6e4ad141.chunk.css"
+  },
+  {
+    "revision": "708064cbe7f7b935fef119c02205a3d4",
+    "url": "/countdown/index.html"
+  }
+];
